e778e05895d78b9194763ff130532a28f09474dc
alexsh882 <alexsh882@gmail.com>
2024-05-03 12:56:28 -0300

    Agregados comentarios fantantes al ejercicio 4 de Matplotlib.

--- matplotlib_4.py
+++ matplotlib_4.py
@@ -11,13 +11,26 @@ import pandas_3
     obtenidas en cada asignatura.
 """
 def mostrar_grafica_notas():
+    """
+    Función muestra un gráfico de barra con los alumnos en el eje de las axisas y
+    los promedios de las notas en el eje de las ordenadas.
+    """
+
+    #creo un DataFrame a partir la función del ejercicio 3
     df = pandas_3.promedio_notas_asignatura_estudiante()
+
+    #creo dos listas con los nombres y los promedios de las notas.
     lista_nombres = list(df["nombres"])
     lista_promedio = list(df["promedio"])
+
+    # creo el gráfico con los datos.
     plt.bar(x=lista_nombres, height=lista_promedio)
     plt.title("Promedio Notas por Estudiante")
     plt.xlabel("Estudiantes")
     plt.ylabel("Notas")
+
+    # mostrar el gráfico creado anteriormente.
     plt.show()
 
+# ejecuto la función.
 mostrar_grafica_notas()
